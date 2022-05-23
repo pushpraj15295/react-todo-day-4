f1f0cc683599516be3a03972a31f085c1cd176df
pushpraj15295 <101568029+pushpraj15295@users.noreply.github.com>
2022-05-23 16:13:41 +0530

    fvsd

--- my-todo/src/comp/Todo.jsx
+++ my-todo/src/comp/Todo.jsx
@@ -1,28 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Todo = () => {
-  
-    const [value,setValue] = useState("");
-    
-    const [todos,setTodos] = useState([]) ; 
+  const [newTodo, setTodo] = useState("");
 
+  const [todos, setTodos] = useState([]);
+
+ const [isCompleteed,setIsCompleteed] = useState(todo.isCompleted)
+
+ const onDelete = () => {
+    
+ }
+  const handalChange = (e) => {
+    setTodo(e.target.value);
+  };
   return (
-    <div>Todo APP
-    
-    <input value={value} onChange={(e) =>setValue(e.target.value)}/>
-    <button onClick={()=> {setTodos([...todos,{value:value}]); setValue("") }} >+</button>
-      
-      {todos.map((todo)=>
-              (<div>
+    <div className="outer">
+      {" "}
+      <h2 className="head">YOUR TODO APP</h2>
+      <div>
+        <input
+          className="inpt1"
+          value={newTodo}
+          type="text"
+          onChange={handalChange}
+        />
 
-                
-                 <div> <input type="checkbox" /> {todo.value}  <button>Remove</button> </div>
-
-              </div>)
-        )}
-
+        <button
+          className="btn"
+          onClick={() => {
+            setTodos([
+              ...todos,
+              { value: newTodo, id: Date.now(), isComplete: false },
+            ]);
+            setTodo("");
+          }}
+        >
+          +
+        </button>
+      </div>
+      <br />
+      <div>
+        {todos.map((todo) => (
+          <div  className="foot" key={todo.id}>
+              
+            <div  className="list">{todo.value}</div>
+            <input type="checkbox" className="checkbox" checked={isCompleted} onChange={(e) => {
+                 setIsCompleted(e.target.checked);
+             }}/>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Todo;
